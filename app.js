2ddccbea89ac0d4b73ef00eb9964c550802ecf2e
Loay Al-Mugharghar <almughargharloay@gmail.com>
2024-10-06 20:57:17 +0300

    Entering words is working

--- app.js
+++ app.js
@@ -1,1 +1,37 @@
 console.log("Hello, World!")
+
+ourWord = [
+  { word: "Beach", Hint: "A place you relax in" },
+  { word: "Booth", Hint: "A place to show your products in" },
+  { word: "Fruit", Hint: "Something Sweet & Natural" },
+]
+// Constants
+
+// Variables (state)
+
+let inputWord
+let correctAns
+let winner
+let lose
+
+//Cached Element References
+
+const enterButton = document.querySelector(".enter")
+
+const messageEl = document.querySelector(".message")
+
+const wordInput = document.querySelector("#wordEntry")
+
+//Functions
+
+const enterInput = () => {
+  inputWord = wordInput.value
+}
+
+const handleClick = (event) => {
+  enterInput()
+}
+
+// Event Listeners
+
+enterButton.addEventListener("click", handleClick)
